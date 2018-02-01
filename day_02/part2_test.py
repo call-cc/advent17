f0f2c5130708511490517c75a3beb5f03b85f28a
Peter Barabas <peter.barabas@notresp.com>
2018-02-01 14:47:07 +0100

    Add more tests for day 2, part 2

--- day_02/part2_test.py
+++ day_02/part2_test.py
@@ -10,6 +10,29 @@ class TestChecksum(unittest.TestCase):
     def setUp(self):
         self.tests = [('example2.txt', 9)]
 
+    def testDivide(self):
+        tests = [(1, 2, 2),
+                 (2, 1, 2),
+                 (2, 8, 4),
+                 (8, 2, 4),
+                 (5, 2, 0),
+                 (2, 5, 0),
+                 (9999, 33, 303)]
+
+        for a, b, result in tests:
+            self.assertEqual(part2.divide(a, b), result)
+
+    def testWorker(self):
+        tests = [([5, 9, 2, 8], 4),
+                 ([2, 5, 9, 8], 4),
+                 ([9, 4, 7, 3], 3),
+                 ([9, 3, 4, 7], 3),
+                 ([3, 8, 6, 5], 2),
+                 ([8, 3, 6, 5], 2)]
+
+        for row, result in tests:
+            self.assertEqual(part2.worker(row[0], row[1:]), result)
+
     def test_checksum(self):
         for filename, result in self.tests:
             ssheet = common.reader(filename)
